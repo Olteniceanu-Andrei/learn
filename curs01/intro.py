c7668af0423621cd468621874e717e634593c613
Olteniceanu Andrei <codewitholty@gmail.com>
2024-09-16 21:32:24 +0300

    intro

---
+++ curs01/intro.py
@@ -0,0 +1,9 @@
+print("Hello")  # comentariu
+print(4 < 5 and 1 > 2)
+
+numar_mere = 5
+numar_pere = 4
+print(f"George are {numar_mere} ani")
+
+variabila = "Ana are {1} cai si {1} boi".format(numar_mere,numar_pere)
+print(variabila)
