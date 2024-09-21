f051185237f314fc42b5ad5a5bef465b704bdecc
Olteniceanu Andrei <codewitholty@gmail.com>
2024-09-21 14:54:54 +0300

    FizzBuzz

---
+++ teme/fizzbuzz.py
@@ -0,0 +1,12 @@
+x = int(input("Scrie un numar: "))
+
+if x % 3 == 0 and x % 5 == 0:
+    print("FizzBuzz")
+elif x % 5 == 0:
+    print("Buzz")
+elif x % 3 == 0:
+    print("Fizz")
+else:
+    print(x)
+
+
