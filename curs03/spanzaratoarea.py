8bd16bc9c6d1ca669269ef8ee5dab88e3c13401f
Olteniceanu Andrei <codewitholty@gmail.com>
2024-09-25 18:53:32 +0300

    update

---
+++ curs03/spanzaratoarea.py
@@ -0,0 +1,12 @@
+cuvant = "onomatopee"
+cuvant_de_inlocuit = ""
+
+l1 = "mancare este buna"
+s1 = "geek"
+
+# creating enumerate objects
+obj1 = enumerate(l1)
+obj2 = enumerate(s1)
+
+
+print (list(enumerate(l1)))
