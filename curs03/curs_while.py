adc09ec4c3fa85732a4618162e18a2d1df0c89af
Olteniceanu Andrei <codewitholty@gmail.com>
2024-09-25 16:53:16 +0300

    update

---
+++ curs03/curs_while.py
@@ -0,0 +1,8 @@
+while True:
+    for i in [1, 2, 3, 4]:
+        print('Ana are mere')
+    break
+
+for i in "Ana are mere":
+    if i in "aeiou":
+        print(i)
---
+++ curs03/exercitii.py
@@ -0,0 +1,6 @@
+nr1 = 4
+if nr1 < 4:
+    total = nr1 + 1
+elif nr1 > 2 and (salariu := "Caca"):
+    total = nr1 + 2
+print(total)
