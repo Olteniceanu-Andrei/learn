56d8e4405363e89087e2bd4aad30e1c82ac5c63b
Olteniceanu Andrei <codewitholty@gmail.com>
2024-11-07 15:37:05 +0200

    ppush

--- curs08/exercitiu1.py
+++ curs08/exercitiu1.py
@@ -1,34 +1,67 @@
+# import pandas as pd
+# import numpy as np
+# import datetime
+#
+# lista = [10, 20, 30, 40, 50]
+# etichete = ['a','b','c','d','e']
+# serie = pd.Series(lista, index=etichete)
+# print('====================')
+# print(serie[serie > 19])
+#
+# data = {"Nume": ['Andrei','Marius','Radu'],
+#         "Varsta": [14, 25, 30],
+#         "Salariu": [0, 5000, 7500]}
+# print('====================')
+# df = pd.DataFrame(data)
+# nume = df["Nume"]
+# print(nume)
+#
+# salariu = df.at[1, "Salariu"]
+# print('====================')
+# print(salariu)
+#
+# df['Experienta'] = ['1 an', '2 ani', '3 ani']
+# print('====================')
+# print(df.iloc[2])
+#
+# df_filtrat = df[(df['Salariu'] > 3000) & (df['Varsta'] > 25)]
+#
+# print(df_filtrat)
+#
+# print('====================')
+#
+# df_sortat = df.sort_values(by="Varsta", ascending=False)
+# print(df_sortat)
+from unittest.mock import inplace
+
 import pandas as pd
-import numpy as np
-import datetime
 
-lista = [10, 20, 30, 40, 50]
-etichete = ['a','b','c','d','e']
-serie = pd.Series(lista, index=etichete)
-print('====================')
-print(serie[serie > 19])
+data = {"Nume": ["Andrei","Viorica","Andrei","George","Andrei"],
+        "Varsta": [14, 21, 25, 50, 14],
+        "Salariu": [0, 12000, 5000, 102000, 1]}
 
-data = {"Nume": ['Andrei','Marius','Radu'],
-        "Varsta": [14, 25, 30],
-        "Salariu": [0, 5000, 7500]}
-print('====================')
 df = pd.DataFrame(data)
-nume = df["Nume"]
-print(nume)
 
-salariu = df.at[1, "Salariu"]
-print('====================')
-print(salariu)
+print(df)
+print("============")
 
-df['Experienta'] = ['1 an', '2 ani', '3 ani']
-print('====================')
-print(df.iloc[2])
+df_faraduplicates = df.drop_duplicates()
 
-df_filtrat = df[(df['Salariu'] > 3000) & (df['Varsta'] > 25)]
+# drop_duplicates functioneaza doar daca toate valorile sunt egale
 
-print(df_filtrat)
+print(df_faraduplicates)
 
-print('====================')
+# Daca sunt valori cu None putem folosi .dropna .fillna
 
-df_sortat = df.sort_values(by="Varsta", ascending=False)
-print(df_sortat)
+df["Experienta"] = [0, 5, 3 , 4 ,10]
+
+print(df)
+
+df.rename(columns={"Experienta": "Nr ani de munca"}, inplace=True)
+
+print(df)
+print=("========")
+df.to_csv("date.csv", index=False)
+
+df22 = pd.read_csv('date.csv')
+print(df22)
